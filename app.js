c39c517c253e8703a7920d41aa6807acaed320ee
yussaq <yussaq.nf@gmail.com>
2022-02-17 15:51:24 +0700

    update form setting

--- app.js
+++ app.js
@@ -3,27 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser= require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap")));
 app.use(express.static(path.join(__dirname, "node_modules/@fortawesome/fontawesome-free/")));
-//app.use(express.static(path.join(__dirname, "node_modules/font-awesome/")));
-
-//app.use(express.static(path.join(__dirname, "node_modules/font-awesome/fonts")));
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
 
 /* Routing */
 app.use('/', require('./routes/index'));
-//app.use('/metadata', require('./routes/metadataRoute'));
-//app.use('/setting', require('./routes/settingRoute');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,5 +35,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
 //  res.render('error');
 });
-
 module.exports = app;
--- controllers/settingController.js
+++ controllers/settingController.js
@@ -2,18 +2,51 @@ const express = require('express');
 const router = express.Router();
 const basePath = process.cwd();
 const fs = require("fs");
-// Json file
 const fileName = `${basePath}/public/asset/json/setting.json`;  
-let data = JSON.parse(fs.readFileSync(fileName));
-
-const getSetting = ((req, res) => {
+const data = JSON.parse(fs.readFileSync(fileName));
+const getSetting = ((req, res, next) => {
+    if (data === undefined || data.length == 0) {
+        const newdata = [{ 
+            id: 1,
+            name: 'my collection',
+            description : 'my best collection',
+            ipfs :'https://',
+            creator: 'Yussaq NF',
+            attributes : 'background'
+        }];
+        data=newdata; 
+    }
     res.render(`${basePath}/views/pages/setting.ejs`, { 
         title:'Setting',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
-        collect :data 
+        setting :data[0]
     });
-})
+});
 
+const updateSetting = ((req, res, next) => {
+    const newdata = { 
+        id: req.body.id,
+        name: req.body.name,
+        description : req.body.description,
+        ipfs :req.body.ipfs,
+        creator: req.body.creator,
+        attributes : req.body.attributes
+    };
+  
+    const formdata = req.body;    
+    const datasetting = [];
+    fs.readFile(fileName, (err, data) => { // get the data from the file
+        if (data === undefined || data.length == 0) {
+            res.send({msg: 'Setting data updated error'})             
+        }
+        datasetting.push(newdata);
+        fs.writeFile(fileName, JSON.stringify(datasetting), (err) => {
+            console.log(err);
+        });            
+    });
+    res.redirect(301,'/attributes');
+})
 module.exports = {
-    getSetting
+    getSetting,
+    updateSetting
 }
