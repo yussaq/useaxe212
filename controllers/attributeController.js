a04ecc3d20d30cdc55f371b1d41ecfe6e80b8537
yussaq <yussaq.nf@gmail.com>
2022-03-02 02:58:40 +0700

    fix delete attributes

--- controllers/attributeController.js
+++ controllers/attributeController.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 const multer = require("multer");
 const fileUpload= require('../middlewares/upload-middleware');
 
+const filePermutation = `${basePath}/public/asset/json/permutation.json`;      
+const dataPermutation = JSON.parse(fs.readFileSync(filePermutation));
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
       cb(null, `${basePath}/public/asset/attributes`);
@@ -61,26 +64,51 @@ const uploadAttribute = ((req, res, next) => {
     res.redirect('/attributes');
 });
 
-/* permtutation from attribute in layer recursive array */
-function getPermutations(layerfiles) {
-    let rowresult = [];        
-  var keyDNA = 'attributes';// hash1;
-  var objDNA = {};
-  if (layerfiles.length == 1) {
-    return layerfiles[0];
-  } else {
-    var result = [];
-    var allCasesOfRest = getPermutations(layerfiles.slice(1)); // recursive array
-    for (var i = 0; i < allCasesOfRest.length; i++) {
-        rowresult = []
-        for (var j = 0; j < layerfiles[0].length; j++) {
-            result.push(layerfiles[0][j] +','+  allCasesOfRest[i]);
+const checkPermutation = (newPermutation) => {
+//    const filePermutation = `${basePath}/public/asset/json/permutation.json`;      
+//    const dataPermutation = JSON.parse(fs.readFileSync(filePermutation));
+    const permutationExists = dataPermutation.some(permutation => permutation.dna === newPermutation.dna);
+    if(permutationExists) {
+        console.log('exists');
+    }else(
+        dataPermutation.push(newPermutation)
+    )
+//    console.log(permutations);
+
+    
+    var permutationJson = `${basePath}/public/asset/json/permutation.json`;   
+    fs.writeFile(permutationJson, JSON.stringify(arraylayer, null, 2), (err) => {
+        if (err)
+            console.log(err);
+        else {
+            res.redirect(301,'/attributes'); 
         }
-        rowresult.push(result[i]);
-    }
+    });
+
+
+}
+
+/* permtutation from attribute in layer recursive array */
+//function getPermutations(layerfiles) {
+const getPermutations = ((layerfiles) => {    
+    let rowresult = [];        
+    var keyDNA = 'attributes';// hash1;
+    var objDNA = {};
+    if (layerfiles.length == 1) {
+        return layerfiles[0];
+    } else {
+        var result = [];
+        var allCasesOfRest = getPermutations(layerfiles.slice(1)); // recursive array
+        for (var i = 0; i < allCasesOfRest.length; i++) {
+            rowresult = []
+            for (var j = 0; j < layerfiles[0].length; j++) {
+                result.push(layerfiles[0][j] +','+  allCasesOfRest[i]);
+            }
+            rowresult.push(result[i]);
+        }
     return result;
   }
-}
+})
 
 /* create data permutation json */
 const permutationAttribute = ((req, res, next) => {  
@@ -89,85 +117,67 @@ const permutationAttribute = ((req, res, next) => {
     const layers = dataSetting[0].attributes;
     layerArray = layers.split(',');
     const layerfiles = [];
-    layerArray.forEach((attribute,index)  => {   
-      try {
-        const arrayOfFiles = fs.readdirSync("./public/asset/attributes/"+attribute);
-        layerfiles[index] = arrayOfFiles;
-      } catch(e) {
-        console.log(e)
-      }
-    });  
+    layerArray.forEach((attribute,index)  => {
+        try {
+            const arrayOfFiles = fs.readdirSync("./public/asset/attributes/"+attribute);
+            layerfiles[index] = arrayOfFiles;
+        } catch(e) {
+            console.log(e)
+        }
+    });
 
     const layerPermutation = getPermutations(layerfiles);
-    var metadataAttributes = []
-    var objAttributes = {}
-    var attributeArray=[]
-    var arraylayer = []
-    let dateTime = Date.now();
-    layerPermutation.forEach((permutation,index)  => {  
-        attributesList = []
-        rowper = []
+    var arraylayer = dataPermutation;
+    layerPermutation.forEach((permutation,index)  => {
+        attributesList = [];
+        rowper = [];
         var vtime = Date.now();
         rowAttribute = permutation.split(',');
-        DNA = '';
-        layerArray.forEach((val,index)  => {  
-            attributesList.push({
-                trait_type: layerArray[index],
-                value: rowAttribute[index].split('.').slice(0, -1).join('.'),            
-            });
-            DNA = DNA+','+rowAttribute[index]
-        })
-        DNA = DNA.substring(1)
-        var hash1 = crypto.createHmac("sha256", DNA).digest("hex")
-        metadataAttributes[hash1]= attributesList;
-        objAttributes.hash1 = attributesList ;
-
-        var key = 'attributes';// hash1;
-        var obj = {};
+        var DNA = crypto.createHmac("sha256", permutation).digest("hex");
         var objlayer = {};
-        obj['name'] = 'my collection';
-        obj['description'] = 'description collection';
-        obj['image'] = 'https://';
-        obj['dna'] = hash1;
-        obj['edition'] = 'edition';
-        obj['date'] = dateTime;
-        obj['creator'] = 'yussaq';
-        obj['status'] = 'pending'; //opt: (generate, upload, publish)   
-        obj['attributes'] = attributesList;
-        obj['compiler'] = 'useaxes212';
-        attributeArray.push(obj);
-
-        var keylayer = hash1;
-        var objlayer = {};
-        objlayer[keylayer] = rowAttribute;
-        arraylayer.push(objlayer);
+        objlayer['dna']= DNA;
+        objlayer['status'] = 'pendding'; //opt: (generate, upload, publish) 
+        objlayer['layers']= layers; //layerArray;
+        objlayer['attributes']= permutation; //rowAttribute;        
+        const permutationExists = dataPermutation.some(permutation => permutation.dna === DNA);
+        if(permutationExists) {
+            console.log('exists ',DNA);
+        }else{
+            arraylayer.push(objlayer);
+        }
     })
-        /* layerPermutation : data to generate layer create collection  */
-        /* metadataAttributes : data to metadata collecton              */
-        var permutationJson = `${basePath}/public/asset/json/permutation.json`;     
+     
+        var permutationJson = `${basePath}/public/asset/json/permutation.json`;   
         fs.writeFile(permutationJson, JSON.stringify(arraylayer, null, 2), (err) => {
             if (err)
-            console.log(err);
+                console.log(err);
             else {
-            res.redirect(301,'/attributes'); 
+                res.redirect(301,'/attributes'); 
             }
         });
-//        res.redirect(301,'/attributes');        
 });
 
 const deleteAttribute = ((req, res, next) => {  
+    const filePermutation = `${basePath}/public/asset/json/permutation.json`;      
+    var newresult = JSON.parse(fs.readFileSync(filePermutation));
     req.body.checkatribute.forEach((item,index)  => {        
         var pathfile = `${basePath}/public/asset/attributes/${req.body.layer}/${item}`;
         console.log(pathfile);
         if (fs.existsSync(pathfile)) {
-            //fs.mkdirSync(`${basePath}/public/asset/attributes/${item}`);
-            //console.log('File deleted!');
             fs.unlink(pathfile, function (err) {
                 if (err) throw err;
-                //console.log('File deleted!');
             }); 
         }
+        result = newresult.filter(({ attributes,status }) => !attributes.includes(item) && status === 'pendding');                newresult = JSON.stringify(result,null,2);
     });  
+
+    var newdata = `${basePath}/public/asset/json/permutation.json`;    
+    fs.writeFile(newdata, newresult, (err) => {
+        if (err)
+        console.log(err);
+    });    
+
+    console.log(newresult);
     res.redirect('/attributes');
 });
 
